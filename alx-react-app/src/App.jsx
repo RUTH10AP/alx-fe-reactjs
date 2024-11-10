0dce2418252a75959acb2176f89388865a972c19
RUTH10AP <wonderuth1@gmailcom>
2024-11-10 10:08:58 -0800

    helo

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile/>
     </div>
   );
 }
