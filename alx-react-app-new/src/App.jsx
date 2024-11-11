2d32fdaaf7eee9e333c4c074d2b099a9d01f49aa
RUTH10AP <wonderuth1@gmailcom>
2024-11-10 17:04:05 -0800

    erewey

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,10 +3,12 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
+      <Counter/>
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
