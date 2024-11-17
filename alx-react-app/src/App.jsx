79963dc7cf346664e0f3447c77b37f63a9869e5a
RUTH10AP <wonderuth1@gmailcom>
2024-11-17 01:37:11 -0800

    newer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,28 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './components/UserProfile';
-import Counter from '../../alx-react-app-new/ALX-react-app-props/src/components/Counter';
+import Counter from '../../alx-react-app-new/ALX-react-app-new/src/components/Counter';
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
+
+
 function App() {
   return (
     <div>
+    <Counter/>
       <Counter/>
       <Header />
       <MainContent />
