f3b9c007eb9010e1a48f339a12f3af58b5fb47cf
RUTH10AP <wonderuth1@gmailcom>
2024-11-17 03:36:35 -0800

    yes

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,10 +3,25 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import UserProfile from './components/UserProfile';
+import Home from '../../my-company/Home';
+import About from '../../my-company/About';
+import Services from '../../my-company/Services';
+import Contact from '../../my-company/Contact';
+import Navbar from '../../my-company/Navbar';
 
 function App() {
   return (
     <div>
+      <Router>
+  <Navbar />
+  {/* Other routes */}
+</Router>
+
+      <Home/>
+      <About/>
+      <Services/>
+      <Contact/>
+      
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
