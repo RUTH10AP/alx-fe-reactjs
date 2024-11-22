fd5a9f5de7877aa8276a518427822c09d7fd25b0
RUTH10AP <wonderuth1@gmailcom>
2024-11-22 08:16:00 -0800

    true

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,19 @@
-// RecipeList component
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom'; 
+  const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
 
-const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
-
-  return (
-    <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+    return (
+      <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+             {/* Using Link to navigate to a detailed page for each recipe */}
+             <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p></Link>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  export default RecipeList
--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -1,7 +1,18 @@
-import create from 'zustand'
-
-const useRecipeStore = create(set => ({
+import { create } from 'zustand';
+export const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  searchTerm: '',  // Initialize searchTerm state
+  setSearchTerm: (term) => set({ searchTerm: term }), // Function to update searchTerm
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));
