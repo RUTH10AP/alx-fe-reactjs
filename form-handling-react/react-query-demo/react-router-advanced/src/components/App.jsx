c9082d5c14e9ede6966e9a4ddc4f68700f24e8e2
RUTH10AP <wonderuth1@gmailcom>
2024-11-24 10:16:58 -0800

    bb

---
+++ form-handling-react/react-query-demo/react-router-advanced/src/components/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as  Router, Route, Routes, Link, useMatch } from 'react-router-dom';
+import BlogPost from './components/BlogPost'; // Import your BlogPost component
+import Home from './components/Profile'; // Example of Profile component
+import ProtectedRoute from './components/ProtectedRoute'; // ProtectedRoute component
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/profile"
+          element={<ProtectedRoute element={<Profile />} />}/>
+        {/* Define the dynamic route for blog posts */}
+        <Route path="/blog/:id" element={<BlogPost />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
