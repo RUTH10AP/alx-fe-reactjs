cb8575ff174b89a6783aeecb5d42f53f2b6b7d9c
RUTH10AP <wonderuth1@gmailcom>
2024-11-30 16:22:06 -0800

    advanced

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileDetails from './ProfileDetails'
+import ProfileSettings from './ProfileSettings'
+import { BrowserRouter as  Router, Route, Routes, Link, useMatch } from 'react-router-dom';
+
+function Profile() {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
