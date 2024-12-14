be4585115cde5054b3cd40544d492aab2cef161f
RUTH10AP <wonderuth1@gmailcom>
2024-12-14 10:41:09 -0800

    hub

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://api.github.com/search/users";
+export const fetchUserData = async (username, location, minRepos, page = 1) => {
+  try {
+    // Construct the query parameters based on the user input
+    let query = `type:user`;
+    
+    if (username) query += `+in:login ${username}`;
+    if (location) query += `+location:${location}`;
+    if (minRepos) query += `+repos:>=${minRepos}`;
 
-export const searchGitHubUsers = async ({ username, location, minRepos }) => {
-  let query = `q=${username}`;
-  if (location) query += `+location:${location}`;
-  if (minRepos) query += `+repos:>=${minRepos}`;
-  
-  const response = await axios.get(`${BASE_URL}?${query}`);
-  return response.data;
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`
+    );
+
+    return response.data;
+  } catch (err) {
+    throw new Error("Error fetching user data");
+  }
 };
